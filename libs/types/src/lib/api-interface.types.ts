de51b345bbf36a5b25f8361f5365049b75e7d803
jason-dark <jason@jasondark.co.nz>
2024-01-13 21:54:37 +1300

    chore: cleanup unused files

--- libs/types/src/lib/api-interface.types.ts
+++ libs/types/src/lib/api-interface.types.ts
@@ -6,3 +6,33 @@ export type CreateJobPayload = Pick<
 >;
 
 export type UpdateJobPayload = Pick<Job, 'id'> & Partial<CreateJobPayload>;
+
+export type RequestWithUser = Request & { user: DecodedUserJwt };
+
+export interface DecodedUserJwt {
+  aud: string;
+  exp: number;
+  iat: number;
+  iss: string;
+  sub: string;
+  email: string;
+  phone: string;
+  app_metadata: AppMetadata;
+  user_metadata: UserMetadata;
+  role: string;
+  aal: string;
+  amr: AMR[];
+  session_id: string;
+}
+
+export interface AMR {
+  method: string;
+  timestamp: number;
+}
+
+export interface AppMetadata {
+  provider: string;
+  providers: string[];
+}
+
+export interface UserMetadata {}
--- libs/types/src/lib/supabase.types.ts
+++ libs/types/src/lib/supabase.types.ts
@@ -3,32 +3,3 @@ import { Database as DB } from './supabase-gen.types';
 type SupabaseJob = DB['public']['Tables']['jobs']['Row'];
 export interface Job extends SupabaseJob {}
 export type Database = DB;
-export type RequestWithUser = Request & { user: DecodedUserJwt };
-
-export interface DecodedUserJwt {
-  aud: string;
-  exp: number;
-  iat: number;
-  iss: string;
-  sub: string;
-  email: string;
-  phone: string;
-  app_metadata: AppMetadata;
-  user_metadata: UserMetadata;
-  role: string;
-  aal: string;
-  amr: AMR[];
-  session_id: string;
-}
-
-export interface AMR {
-  method: string;
-  timestamp: number;
-}
-
-export interface AppMetadata {
-  provider: string;
-  providers: string[];
-}
-
-export interface UserMetadata {}
