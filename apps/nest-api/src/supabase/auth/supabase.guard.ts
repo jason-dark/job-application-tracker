6ecc47452c9797729b5e2a4b3b0a0295aca4cf22
Jason Dark <jason@jasondark.co.nz>
2024-01-13 21:59:15 +1300

    Feat/add nest logging (#2)
    * feat: add improved api logging
    
    * chore: cleanup unused files

--- apps/nest-api/src/supabase/auth/supabase.guard.ts
+++ apps/nest-api/src/supabase/auth/supabase.guard.ts
@@ -1,10 +1,19 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { DecodedUserJwt } from '@job-application-tracker/types';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
+
   constructor(private readonly configService: ConfigService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -14,7 +23,11 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const user = jwt.verify(token, this.configService.get('SUPABASE_JWT_SECRET')) as JwtPayload;
+      const user = jwt.verify(
+        token,
+        this.configService.get('SUPABASE_JWT_SECRET')
+      ) as DecodedUserJwt;
+      this.logger.log(`User ${user.email} verified by jwt`);
       // Assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = user;
--- libs/types/src/lib/supabase.types.ts
+++ libs/types/src/lib/supabase.types.ts
@@ -1,8 +1,5 @@
-import { JwtPayload } from 'jsonwebtoken';
-
 import { Database as DB } from './supabase-gen.types';
 
 type SupabaseJob = DB['public']['Tables']['jobs']['Row'];
 export interface Job extends SupabaseJob {}
 export type Database = DB;
-export type RequestWithUser = Request & { user?: JwtPayload };
