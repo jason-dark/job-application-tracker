b8e6e8ba4ce122ac42f582a5b18e25ea0cb44aac
jason-dark <jason@jasondark.co.nz>
2024-01-13 18:24:48 +1300

    refactor: use cors origin string

--- apps/nest-api/src/main.ts
+++ apps/nest-api/src/main.ts
@@ -11,11 +11,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      process.env.NEXT_PUBLIC_URL,
-      process.env.NODE_ENV === 'development' ? 'http://localhost:4200' : '',
-    ],
+    origin: process.env.NEXT_PUBLIC_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    credentials: true,
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
